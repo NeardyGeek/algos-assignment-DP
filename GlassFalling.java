b6a4903d0160b5492c77a1bbbba142e9b27e89fe
Gen Li <gen.li2@outlook.com>
2019-03-31 20:28:10 -0400

    updated

--- GlassFalling.java
+++ GlassFalling.java
@@ -6,7 +6,26 @@ public class GlassFalling {
   // Do not change the parameters!
   public int glassFallingRecur(int floors, int sheets) {
     // Fill in here and change the return
-    return 0;
+	  
+	  if(floors==0|| floors==1) {
+		  return floors;
+	  }
+	  if(sheets==1) {
+		  return floors;
+	  }
+	  
+	  	
+	   int result= Integer.MAX_VALUE, temp;
+	  
+	  for(int i=1; i<=floors; i++) {
+		   	temp = Math.max(glassFallingRecur(i-1,sheets-1), glassFallingRecur(floors-i,sheets));
+		    result= Math.min(temp, result);
+	  }
+	  
+	
+	  return result+1;
+	  
+	 
   }
 
   // Optional:
@@ -19,13 +38,43 @@ public class GlassFalling {
   // Do not change the parameters!
   public int glassFallingBottomUp(int floors, int sheets) {
     // Fill in here and change the return
-    return 0;
+	  int [][] minTrials= new int[sheets+1][floors+1];
+	  
+	  for (int row=0; row<=sheets; row++) {
+		  minTrials[row][0]=0;
+	  }
+	  
+	  for(int col=0; col<=floors; col++) {
+		  minTrials[0][col]=0;
+	  }
+	  
+	  for(int floor=1; floor<=floors; floor++) {
+		  minTrials[1][floor]=floor;
+	  }
+	  
+	  for(int sheet=1; sheet<=sheets; sheet++) {
+		  minTrials[sheet][1]=1;
+	  }
+	 
+	  for(int sheet=2; sheet<=sheets; sheet++) {
+		  for(int floor=2; floor<=floors; floor++) {
+			  minTrials[sheet][floor]= Integer.MAX_VALUE;
+			  for(int i=1; i<=floor;i++) {
+				  int temp = Math.max(minTrials[sheet-1][i-1], minTrials[sheet][floor-i])+1;
+				  minTrials[sheet][floor]= Math.min(minTrials[sheet][floor],temp);
+				 
+			  }
+			  
+		  }
+	  }
+	  
+    return minTrials[sheets][floors];
   }
 
 
   public static void main(String args[]){
       GlassFalling gf = new GlassFalling();
-
+      
       // Do not touch the below lines of code, and make sure
       // in your final turned-in copy, these are the only things printed
       int minTrials1Recur = gf.glassFallingRecur(27, 2);
@@ -34,5 +83,7 @@ public class GlassFalling {
       int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
       System.out.println(minTrials1Recur + " " + minTrials1Bottom);
       System.out.println(minTrials2Recur + " " + minTrials2Bottom);
+     
+      
   }
 }
--- RodCutting.java
+++ RodCutting.java
@@ -5,12 +5,57 @@ public class RodCutting {
 
   // Do not change the parameters!
   public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
+	  int [] r = new int[rodLength+1];
+	  for(int i=0; i<rodLength+1; i++) {
+		  r[i]= Integer.MIN_VALUE;
+	  }
+	  
+	  
+	  
+    return rodCuttingMemo(rodLength, lengthPrices, r);
+  }
+  
+  public int rodCuttingMemo(int rodLength, int[] lengthPrices, int[] r) {
+	  	
+	  	
+	 if(r[rodLength]>=0) {
+		 return r[rodLength];
+	 }
+	  
+	  int ans=0;
+	  if(rodLength==0) {
+		   ans = 0;
+	  }else {
+		   ans= Integer.MIN_VALUE;
+		   
+		  
+	  }
+	  
+	  for(int i=0; i<rodLength; i++) {
+		  ans= Math.max(ans, lengthPrices[i]+rodCuttingMemo(rodLength-i-1, lengthPrices, r));
+		 r[rodLength]=ans;
+	  }
+	 
+	  
+	  return ans;
   }
 
   // Do not change the parameters!
   public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
+	  int [] r = new int[rodLength+1];
+	  r[0]=0;
+	  
+	  for(int length=1; length<rodLength+1; length++) {
+		  int ans= Integer.MIN_VALUE;
+		  for(int i=0; i<length; i++) {
+			  ans= Math.max(ans, lengthPrices[i]+r[length-i-1]);
+			 
+		  }
+		  r[length]=ans;
+	  }
+	  
+	  
+	  return r[rodLength];
   }
 
 
@@ -24,10 +69,11 @@ public class RodCutting {
       int length2 = 14;
       int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
       int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
-      int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
+     int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
       int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
       int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
-      System.out.println(maxSell1Recur + " " + maxSell1Bottom);
-      System.out.println(maxSell2Recur + " " + maxSell2Bottom);
+     System.out.println(maxSell1Recur + " " + maxSell1Bottom);
+     System.out.println(maxSell2Recur + " " + maxSell2Bottom);
+      
   }
 }
